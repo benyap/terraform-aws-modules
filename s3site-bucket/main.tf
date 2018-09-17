41431fb6c2d6e44d03650cd5cda3b52d5691586b
Ben Yap <bwyap@outlook.com>
2018-09-17 20:46:38 +1000

    feat: add s3site-bucket module

---
+++ s3site-bucket/main.tf
@@ -0,0 +1,29 @@
+#######################
+# Configuration
+#######################
+
+# Create policy which allows public access to bucket contents
+data "aws_iam_policy_document" "s3site-bucket-policy" {
+  statement {
+    actions = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${var.bucket_name}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:UserAgent"
+      values = ["${var.duplicate_content_penalty_secret}"]
+    }
+  }
+}
+
+# Create the S3 bucket for static website hosting
+resource "aws_s3_bucket" "s3site-bucket" {
+  bucket    = "${var.bucket_name}"
+  policy    = "${data.aws_iam_policy_document.s3site-bucket-policy.rendered}"
+  region    = "${var.region}"
+
+  website {
+    index_document  = "${var.index_document}"
+    error_document  = "${var.error_document}"
+    routing_rules   = "${var.routing_rules}"
+  }
+}
---
+++ s3site-bucket/vars.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  description = "The region the website bucket will reside in"
+}
+
+variable "bucket_name" {
+  description = "The name of the bucket that holds the website assets"
+}
+
+variable "duplicate_content_penalty_secret" {
+  description = "Value that will be used in a custom header for a CloudFront distribution to gain access to the origin S3 bucket"
+}
+
+variable "index_document" {
+  description = "The name of the index file for the website"
+}
+
+variable "error_document" {
+  description = "The name of the error file for the website"
+}
+
+variable "routing_rules" {
+  description = "Routing rules for the S3 website"
+  default = {}
+}
