091cf7311fd80e309dd0954c7431d9ce7c35fa4e
Ben Yap <bwyap@outlook.com>
2018-09-18 13:45:42 +1000

    feat: add tags to s3site-bucket module

--- s3site-bucket/main.tf
+++ s3site-bucket/main.tf
@@ -30,6 +30,8 @@ resource "aws_s3_bucket" "s3site-bucket" {
   region    = "${var.aws_region}"
   acl       = "public-read"
 
+  tags = "${merge("${var.tags}", map("Name", "${var.project_tag}-${var.environment_tag}-${var.domain_tag}", "Environment", "${var.environment_tag}", "Project", "${var.project_tag}"))}"
+
   website {
     index_document  = "${var.index_document}"
     error_document  = "${var.error_document}"
