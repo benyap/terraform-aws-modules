19fe69dc32baf30ec29edded1f4d7570726e60e8
Ben Yap <bwyap@outlook.com>
2018-09-19 23:23:40 +1000

    fix: invalid variable reference

--- cloudfront-distribution/outputs.tf
+++ cloudfront-distribution/outputs.tf
@@ -1,13 +1,13 @@
 output "cdn_domain_name" {
   value = "${var.logging_enabled ?
-    element(aws_cloudfront_distribution.domain-cdn-with-logging.*.domain_name, 0) :
-    element(aws_cloudfront_distribution.domain-cdn.*.domain_name, 0)
+    element(concat(aws_cloudfront_distribution.domain-cdn-with-logging.*.domain_name, list("")), 0) :
+    element(concat(aws_cloudfront_distribution.domain-cdn.*.domain_name, list("")), 0)
   }"
 }
 
 output "hosted_zone_id" {
   value = "${var.logging_enabled ?
-    element(aws_cloudfront_distribution.domain-cdn-with-logging.*.hosted_zone_id, 0) :
-    element(aws_cloudfront_distribution.domain-cdn.*.hosted_zone_id, 0)
+    element(concat(aws_cloudfront_distribution.domain-cdn-with-logging.*.hosted_zone_id, list("")), 0) :
+    element(concat(aws_cloudfront_distribution.domain-cdn.*.hosted_zone_id, list("")), 0)
   }"
 }
