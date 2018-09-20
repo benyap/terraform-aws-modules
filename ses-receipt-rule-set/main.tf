d73cfa7a046288165ac07191f6d59921e74b4096
Ben Yap <bwyap@outlook.com>
2018-09-20 18:12:14 +1000

    fix: use provider in us-east-1 for ses-reciept-rule-set module

--- ses-receipt-rule-set/main.tf
+++ ses-receipt-rule-set/main.tf
@@ -1,3 +1,23 @@
+#######################
+# Setup
+#######################
+
+# Create AWS provider in us-east-1
+provider "aws" {
+  region  = "us-east-1"
+  version = "~>1.36"
+
+  # Assume the terraform role to give access to AWS resources.
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_id}:role/${var.role_name}"
+  }
+}
+
+
+########################
+# Configuration
+########################
+
 resource "aws_ses_receipt_rule_set" "rule-set" {
   rule_set_name = "${var.rule_set_name}"
 }
