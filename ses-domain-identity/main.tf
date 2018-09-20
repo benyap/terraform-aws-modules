8224164a6195bd97419decffea0ff7de82588482
Ben Yap <bwyap@outlook.com>
2018-09-20 10:57:18 +1000

    fix: add provider in us-east-1

--- ses-domain-identity/main.tf
+++ ses-domain-identity/main.tf
@@ -1,14 +1,30 @@
-########################
-# Create the SES domain
-########################
-resource "aws_ses_domain_identity" "ses-domain-identity" {
-  domain = "${var.email_domain}"
+#######################
+# Setup
+#######################
+
+# Create AWS provider in us-east-1
+provider "aws" {
+  region  = "us-east-1"
+  alias   = "use1"
+  version = "~>1.36"
+
+  # Assume the terraform role to give access to AWS resources.
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_id}:role/${var.role_name}"
+  }
 }
 
 
 ########################
-# Create verification record
+# Configuration
 ########################
+
+# Create the SES domain
+resource "aws_ses_domain_identity" "ses-domain-identity" {
+  domain = "${var.email_domain}"
+}
+
+# Create verification record
 resource "aws_route53_record" "ses-domain-identity-verification-record" {
   zone_id = "${var.hosted_zone_id}"
   name    = "_amazonses.${var.email_domain}"
@@ -17,17 +33,13 @@ resource "aws_route53_record" "ses-domain-identity-verification-record" {
   records = ["${aws_ses_domain_identity.ses-domain-identity.verification_token}"]
 }
 
-########################
 # Verify using Route53
-########################
 resource "aws_ses_domain_identity_verification" "domain_verification" {
   domain = "${aws_ses_domain_identity.ses-domain-identity.id}"
   depends_on = ["aws_route53_record.ses-domain-identity-verification-record"]
 }
 
-########################
 # Set up DKIM and Route53 records
-########################
 resource "aws_ses_domain_dkim" "ses-domain-dkim" {
   domain = "${aws_ses_domain_identity.ses-domain-identity.domain}"
 }
@@ -41,10 +53,7 @@ resource "aws_route53_record" "domain-amazonses-verification-record" {
   records = ["${element(aws_ses_domain_dkim.ses-domain-dkim.dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
 
-########################
 # Set up MX inbound Route53 record
-########################
-
 resource "aws_route53_record" "ses-domain-mx-inbound-record" {
   zone_id = "${var.hosted_zone_id}"
   name    = "${var.email_domain}"
--- ses-domain-identity/variables.tf
+++ ses-domain-identity/variables.tf
@@ -1,3 +1,11 @@
+variable "account_id" {
+  description = "The account ID"
+}
+
+variable "role_name" {
+  description = "The name of the role to assume to create the SES domain identity"
+}
+
 variable "email_domain" {
   description = "The custom email domain to use for SES"
 }
