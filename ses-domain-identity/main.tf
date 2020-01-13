22eb0b9dafd8c376348ed3ee1fa3e1a8071e387f
Ben Yap <bwyap@outlook.com>
2020-01-13 18:06:49 +1100

    feat: upgrade some modules to Terraform v0.12

--- ses-domain-identity/main.tf
+++ ses-domain-identity/main.tf
@@ -13,13 +13,13 @@ resource "aws_route53_record" "ses-domain-identity-verification-record" {
   name    = "_amazonses.${var.email_domain}"
   type    = "TXT"
   ttl     = "600"
-  records = ["${aws_ses_domain_identity.ses-domain-identity.verification_token}"]
+  records = [aws_ses_domain_identity.ses-domain-identity.verification_token]
 }
 
 # Verify using Route53
 resource "aws_ses_domain_identity_verification" "domain_verification" {
   domain     = aws_ses_domain_identity.ses-domain-identity.id
-  depends_on = ["aws_route53_record.ses-domain-identity-verification-record"]
+  depends_on = [aws_route53_record.ses-domain-identity-verification-record]
 }
 
 # Set up DKIM and Route53 records
